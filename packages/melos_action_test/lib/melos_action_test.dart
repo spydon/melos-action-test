ef92f47e928c0fa1ede384daefa2f582a837c49c
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2023-11-09 18:40:57 +0100

    feat: Hopefully the final test

--- packages/melos_action_test/lib/melos_action_test.dart
+++ packages/melos_action_test/lib/melos_action_test.dart
@@ -2,6 +2,6 @@ library melos_action_test;
 
 /// A Calculator.
 class Calculator {
-  /// Returns [value] plus 11.
-  int addOne(int value) => value + 11;
+  /// Returns [value] plus 12.
+  int addOne(int value) => value + 12;
 }

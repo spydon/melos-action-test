e1ef9a9e4d0e6fb276ba91b1716a1475d98ea687
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2023-11-09 15:33:53 +0100

    feat: Now the calculator is even more powerful +4

--- packages/melos_action_test/lib/melos_action_test.dart
+++ packages/melos_action_test/lib/melos_action_test.dart
@@ -2,6 +2,6 @@ library melos_action_test;
 
 /// A Calculator.
 class Calculator {
-  /// Returns [value] plus 4.
-  int addOne(int value) => value + 4;
+  /// Returns [value] plus 5.
+  int addOne(int value) => value + 5;
 }

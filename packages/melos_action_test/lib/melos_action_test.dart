be68f20ddb8f2290143b6285f9a896c55050235f
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2023-11-09 14:17:08 +0100

    feat: Very important new feature test

--- packages/melos_action_test/lib/melos_action_test.dart
+++ packages/melos_action_test/lib/melos_action_test.dart
@@ -2,6 +2,6 @@ library melos_action_test;
 
 /// A Calculator.
 class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+  /// Returns [value] plus 2.
+  int addOne(int value) => value + 2;
 }
